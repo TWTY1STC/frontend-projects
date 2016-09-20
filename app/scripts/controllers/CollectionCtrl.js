521767291e9d4455ff017c02db3e49ceb8b5752e
twty1stc <liza.njuguna@gmail.com>
2016-09-20 18:06:27 +0000

    ongoing assignment work

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -5,6 +5,7 @@
             this.albums.push(angular.copy(albumPicasso));
         }
     }
+    //how to link fixtures.js to this so albumPicasso means something?
     
     angular
         .module('blocJams')
