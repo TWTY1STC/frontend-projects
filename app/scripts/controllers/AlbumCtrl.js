1581018bddf55f11f231f352f4f59c3eb19fd910
twty1stc <liza.njuguna@gmail.com>
2016-09-21 18:31:02 +0000

    add factory service to album controller

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,14 +1,13 @@
 (function(){
-    function AlbumCtrl(){
+    function AlbumCtrl(Fixtures){
         //Add an albumData property that holds a copy of albumPicasso.
         //how to link fixtures.js to this so albumPicasso means something?
-
-        this.albumData = angular.copy(albumPicasso)
-        console.log(albumPicasso);
+            //by adding a service.
+        this.albumData = Fixtures.getAlbum();
         
     }
     
     angular
         .module("blocJams")
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
