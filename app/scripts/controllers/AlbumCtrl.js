101ccb687cefa1af5ef4331de3ee7e5a61998f0e
twty1stc <liza.njuguna@gmail.com>
2016-09-21 20:45:50 +0000

    add song play/pause  functionality - ongoing

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,16 @@
 (function(){
-    function AlbumCtrl(Fixtures){
+    function AlbumCtrl(Fixtures, SongPlayer){
         //Add an albumData property that holds a copy of albumPicasso.
         //how to link fixtures.js to this so albumPicasso means something?
             //by adding a service.
+            
+            
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
         
     }
     
     angular
         .module("blocJams")
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', "SongPlayer", AlbumCtrl]);
 })();
---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,44 @@
+(function(){
+    function SongPlayer(){
+        var SongPlayer = {};    //empty object
+                                //the service returns this and makes the properties and methods available to the rest of the application.
+        var currentSong = null;
+        var currentBuzzObject = null;
+        
+        SongPlayer.play = function(song) {
+            if (currentSong !== song){
+                if(currentBuzzObject){ //click on a different song than the one that's playing
+                    currentBuzzObject.stop();
+                    currentSong.playing = null;
+                }
+                
+                currentBuzzObject = new buzz.sound(song.audioUrl, {
+                    formats: ['mp3'],
+                    preload: true  
+                });
+                
+                currentSong = song; 
+            
+                currentBuzzObject.play();
+                song.playing = true; 
+            }
+            else if (currentSong === song){
+                if(currentBuzzObject.isPaused()){
+                    currentBuzzObject.play();
+                }
+            }
+        };
+        
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.puase();
+            song.playing = false;
+        };
+        
+        return SongPlayer;
+        
+    }
+    
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
