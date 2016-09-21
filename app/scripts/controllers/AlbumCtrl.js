6c386999772f105abc220f61d1f46c2931c707eb
twty1stc <liza.njuguna@gmail.com>
2016-09-21 17:51:29 +0000

    add album controller

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,29 +2,10 @@
     function AlbumCtrl(){
         //Add an albumData property that holds a copy of albumPicasso.
         //how to link fixtures.js to this so albumPicasso means something?
-<<<<<<< HEAD
-        
-        this.albumData = angular.copy(albumPicasso)
-       
-=======
 
-        var albumData = angular.copy(albumPicasso)
-        var albumData = albumPiscasso = {
-            title: 'The Colors',
-            artist: 'Pablo Picasso',
-            label: 'Cubism',
-            year: '1881',
-            albumArtUrl: 'assets/images/album_covers/01.png',
-            songs: [
-                { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue'},
-                { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-                { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-                { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink'},
-                { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta'}
-            ]
-        };
+        this.albumData = angular.copy(albumPicasso)
+        console.log(albumPicasso);
         
->>>>>>> master
     }
     
     angular
