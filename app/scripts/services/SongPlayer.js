dfcc2ee19fb32fba06e3575e332d0f0cb991bc55
twty1stc <liza.njuguna@gmail.com>
2016-09-28 16:25:45 +0000

    add playback tracking functionality

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function(){
-    function SongPlayer(Fixtures){
+    function SongPlayer($rootScope, Fixtures){
         var SongPlayer = {}; //empty object, the service returns this and makes the properties and methods available to the rest of the application.
         
         /**
@@ -29,6 +29,12 @@
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true  
+            });
+            
+            currentBuzzObject.bind('timeupdate', function(){
+                $rootScope.$apply(function(){
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
             });
             
             SongPlayer.currentSong = song; 
@@ -68,6 +74,12 @@
          * @type {Object} song
         */
         SongPlayer.currentSong = null; 
+        
+        /**
+         * @desc Current playback time(in seconds) of currently playing song
+         * @type {Number}
+         */
+        SongPlayer.currentTime = null;
         
         /**
          * @name SongPlayer.play
@@ -130,6 +142,19 @@
                 playSong(song);
             }
         };
+        /**
+         * @function setCurrentTime
+         * @desc set current time in seconds of currently playing song
+         * @param {Number} time
+         *
+         */
+        SongPlayer.setCurrentTime = function(time){
+            if(currentBuzzObject){
+                currentBuzzObject.setTime(time);
+            }
+        };
+        
+        SongPlayer.currentAlbum = currentAlbum;
          
         return SongPlayer;
         
@@ -137,5 +162,5 @@
     
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
